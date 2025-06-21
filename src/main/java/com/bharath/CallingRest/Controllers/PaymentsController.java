36da9f13c19d9c974b658de7c74f14c849f28019
bharathch <bharrathch@gmail.com>
2025-06-21 16:08:47 +0530

    First Commit

--- src/main/java/com/bharath/CallingRest/Controllers/PaymentsController.java
+++ src/main/java/com/bharath/CallingRest/Controllers/PaymentsController.java
@@ -1,12 +1,16 @@
 package com.bharath.CallingRest.Controllers;
 
 import com.bharath.CallingRest.Models.Payment;
+import com.bharath.CallingRest.Models.Purchase;
+import com.bharath.CallingRest.Repositories.PurchaseRepository;
 import com.bharath.CallingRest.client.PaymentProxy;
 import com.bharath.CallingRest.client.PaymentRestProxy;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.UUID;
 import java.util.logging.Logger;
 
@@ -16,6 +20,8 @@ public class PaymentsController {
     private static Logger logger=Logger.getLogger(PaymentsController.class.getName());
     private final PaymentProxy paymentProxy;
     private final PaymentRestProxy paymentRestProxy;
+    @Autowired
+    private PurchaseRepository purchaseRepository;
 
     public PaymentsController(PaymentProxy paymentProxy,PaymentRestProxy prp){
         this.paymentProxy=paymentProxy;
@@ -34,6 +40,18 @@ public class PaymentsController {
                 .status(HttpStatus.OK)
                 .header("request ID",requestId)
                 .body(retPay);
+    }
+
+    @PostMapping("/purchase")
+    public ResponseEntity purchase(@RequestBody Purchase purchase){
+        purchaseRepository.storePurchase(purchase);
+        return null;
+    }
+
+    @GetMapping("/purchase")
+    public ResponseEntity<List<Purchase>> Purchases(){
+        var purchaseList=purchaseRepository.getPurchases();
+        return  ResponseEntity.status(HttpStatus.OK).body(purchaseList);
     }
 
 
